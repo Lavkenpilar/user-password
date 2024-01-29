a198a5852844471e1941c2c9cfa43aff74e474cf
Lavken <lavkenpilar@gmail.com>
2024-01-28 21:54:34 -0300

    create index-assets-js

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const user = prompt ("email")
+const password = prompt("contraseña")
+if (user == "patriciapilarhern@gmail.com" && password=="desafiolatam") {
+    alert ("usuario correcto")
+} else {
+    alert ("usuario incorrecto")
+}
